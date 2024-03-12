a28ae66ee85c8223e043050ebc911623b89b3910
kade <zsc0248@gmail.com>
2024-03-13 05:49:00 +0900

    feat : playGame까지 구현

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import Service.BowlingService;
+import Service.Player;
+import Util.InputData;
+import Util.Message;
+
+
+import java.util.List;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Message message = new Message();
+        BowlingService bowlingService = new BowlingService();
+        InputData inputData = new InputData();
+
+        message.introImage();
+
+        int people = inputData.numberOfPlayers();
+        List<Player> playerList = bowlingService.makePlayerList(people);
+
+        bowlingService.playGame(playerList);
+
+        /*for (Player player : playerList) {
+            System.out.println(Arrays.toString(player.getScoreboard()));
+        }*/
+
     }
 }
