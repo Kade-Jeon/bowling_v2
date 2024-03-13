03a05e8e6c355335194f245a18b2c0e6fa091758
kade <zsc0248@gmail.com>
2024-03-14 04:40:51 +0900

    Feat/Refactor : 1. 점수 계산 로직 수정 2. 출력 부분 Message 클래스를 통한 구현으로 수정 3. 순위 로직 구현

--- src/Service/BowlingService.java
+++ src/Service/BowlingService.java
@@ -10,15 +10,22 @@ public class BowlingService {
 
     private final InputData inputData = new InputData();
 
-    public List<Player> makePlayerList(int people) {
+
+    public List<Player> makePlayerList(int numberOfPlayer) {
         List<Player> playerList = new ArrayList<>();
-        for (int i = 0; i < people; i++) {
+        for (int i = 0; i < numberOfPlayer; i++) {
             Player player = new Player("Player" + (i + 1));
             playerList.add(player);
         }
         return playerList;
     }
 
+    /**
+     * playGame() : 게임 진행 로직
+     * 아래 두 가지 케이스로 나누어서 구현
+     * 1. 1 ~ 9 프레임 케이스
+     * 2. 10 프레임 케이스
+     * */
     public void playGame(List<Player> playerList) {
         for (int frame = 0; frame < 10; frame++) {
             Message.showRound(frame + 1);
@@ -37,20 +44,20 @@ public class BowlingService {
                             player.setFrameScore(frame, first);
                             break;
                         }
+
                         if (first < 10) { // STRIKE 실패
                             Message.askSpare(player);
                             int spare = inputData.numberOfPin();
                             if (0 > spare || spare > 10 || first + spare > 10) { // Spare 입력 오류
                                 throw new InputOutOfRangeException("스페어 결과는 0 ~ " + (10 - first) + " 사이의 값만 입력 가능합니다. \n재입력 바랍니다.");
                             }
-
                             player.setFrameScore(frame, first, spare);
                             break;
                         }
                     } catch (InputOutOfRangeException e) {
-                        System.out.println(e.getMessage());
+                        Message.InputOutOfRangeExceptionPrint(e.getMessage());
                     } catch (InputMismatchException e) {
-                        System.out.println("숫자만 입력 가능합니다.");
+                        Message.InputMismatchExceptionPrint();
                         inputData.cleanBuffer(); // 버퍼를 비워 주지 않으면 무한 반복이 일어 난다고 함...
                     }
                 }//while - 1~9 Frame  END
@@ -64,7 +71,6 @@ public class BowlingService {
                         }
 
                         int spare = 0;
-
                         if (first == 10) {
                             Message.askSpare(player);
                             spare = inputData.numberOfPin();
@@ -80,7 +86,6 @@ public class BowlingService {
                                 throw new InputOutOfRangeException("스페어 결과는 0 ~ " + (10 - first) + " 사이의 값만 입력 가능합니다. \n재입력 바랍니다.");
                             }
                         }
-
 
                         if (first + spare == 20 || first + spare == 10) {
                             Message.askBonus(player);
@@ -108,95 +113,111 @@ public class BowlingService {
                         }
 
                     } catch (InputOutOfRangeException e) {
-                        System.out.println(e.getMessage());
+                        Message.InputOutOfRangeExceptionPrint(e.getMessage());
                     } catch (InputMismatchException e) {
-                        System.out.println("숫자만 입력 가능합니다.");
+                        Message.InputMismatchExceptionPrint();
                         inputData.cleanBuffer(); // 버퍼를 비워 주지 않으면 무한 반복이 일어 난다고 함...
                     }
                 }//while - 10 Frame  END
-                System.out.println(Arrays.toString(player.getScoreboard()));
                 scoreCalculator(player, frame);
-                System.out.println(Arrays.toString(player.getTotalScore()));
+                Message.showDisplayFrame(playerList);
             }//for - playerList END
         }//for - frame END
     }
 
+    /**
+     * scoreCalculator() : 점수 계산 로직으로 외부에서는 호출할 필요가 없어 private으로 구현
+     * */
     private void scoreCalculator(Player player, int frame) {
         try {
-            if (player.getFrameScore(frame).size() == 2 && player.getFrameScore(frame).get(0) + player.getFrameScore(frame).get(1) < 10) { // 스페어 실패 : 현 초구 + 현 스페어 합
-                //System.out.println("시작" + frame);
+            // 1. 현 프레임이 스페어 실패인 경우
+            if (player.getFrameScore(frame).size() == 2 && player.getFrameScore(frame).get(0) + player.getFrameScore(frame).get(1) < 10) {
+                player.setFrameTotalScore(frame,
+                            player.getFrameTotalScore(frame - 1)
+                                + player.getFrameScore(frame).get(0)
+                                + player.getFrameScore(frame).get(1));
 
+                // 1-1. 이전 프레임이 스페어 실패
                 if (player.getFrameScore(frame - 1).size() == 2 && player.getFrameScore(frame - 1).get(0) + player.getFrameScore(frame - 1).get(1) < 10) {
-                    //System.out.println("직전 2샷 - 실패");
                     player.setFrameTotalScore(frame - 1,
-                            player.getFrameTotalScore(frame - 2)
+                                player.getFrameTotalScore(frame - 2)
                                     + player.getFrameScore(frame - 1).get(0)
                                     + player.getFrameScore(frame - 1).get(1));
-
                 }
+
+                // 1-2. 이전 프레임이 스페어 성공
                 if (player.getFrameScore(frame - 1).size() == 2 && player.getFrameScore(frame - 1).get(0) + player.getFrameScore(frame - 1).get(1) == 10) {
-                    //System.out.println("직전 2샷 - 성공");
                     player.setFrameTotalScore(frame - 1,
-                            player.getFrameTotalScore(frame - 2)
+                                player.getFrameTotalScore(frame - 2)
                                     + player.getFrameScore(frame - 1).get(0)
                                     + player.getFrameScore(frame - 1).get(1)
                                     + player.getFrameScore(frame).get(0));
                 }
 
+                // 2. 이전 프레임이 스트라이크
                 if (player.getFrameScore(frame - 1).size() == 1) {
-                    //System.out.println("직전 1샷");
                     player.setFrameTotalScore(frame - 1,
-                            player.getFrameTotalScore(frame - 2)
+                                player.getFrameTotalScore(frame - 2)
                                     + player.getFrameScore(frame - 1).get(0)
                                     + player.getFrameScore(frame).get(0));
                 }
+
+
+                /*
+                  맨 처음과 코드가 중복인데 이는 아래와 같은 이유가 있다
+                  1. 1 프레임과 같이 이전 값이 없을 경우 바로 현재 프레임 스코어를 계산해야 하기 때문에 맨 위에 중복코드가 있고
+                  2. 이 코드는 위의 조건을 거쳐 온 후에 현재 프레임 스코어를 다시 계산해야 하기 때문에 있다.
+                  => 예외가 발생하면 발생한 곳 이후의 코드가 실행되지 않기 때문에 2가 없으면 1의 경우에 계산이 되지 않는 문제가 있기 때문.
+                */
                 player.setFrameTotalScore(frame,
-                        player.getFrameTotalScore(frame - 1)
+                            player.getFrameTotalScore(frame - 1)
                                 + player.getFrameScore(frame).get(0)
                                 + player.getFrameScore(frame).get(1));
             }
 
-            if (player.getFrameScore(frame - 1).size() == 1 && player.getFrameScore(frame).size() == 2) { // 직전 스트라이크
-                //System.out.println("중간" + frame);
+            // 이전 프레임이 스트라이크인 경우
+            if (player.getFrameScore(frame - 1).size() == 1 && player.getFrameScore(frame).size() == 2) {
                 player.setFrameTotalScore(frame - 1,
-                        player.getFrameTotalScore(frame - 2)
+                            player.getFrameTotalScore(frame - 2)
                                 + player.getFrameScore(frame - 1).get(0)
                                 + player.getFrameScore(frame).get(0)
                                 + player.getFrameScore(frame).get(1));
-                if (player.getFrameScore(frame).get(0) + player.getFrameScore(frame).get(1) < 10) { // 스페어 실패
-                    //System.out.println("스페어 실패");
+
+                // 현재 프레임 스페어 실패
+                if (player.getFrameScore(frame).get(0) + player.getFrameScore(frame).get(1) < 10) {
                     player.setFrameTotalScore(frame,
-                            player.getFrameTotalScore(frame - 1)
+                                player.getFrameTotalScore(frame - 1)
                                     + player.getFrameScore(frame).get(0)
                                     + player.getFrameScore(frame).get(1));
                 }
             }
 
-            if (player.getFrameScore(frame - 2).size() == 1 && player.getFrameScore(frame - 1).size() == 1) { // 전전 스트라이크 직전 스트라이크
-                //System.out.println("끝" + frame);
+            // 3. 전전 스트라이크 + 전 스트라이크 (더블)
+            if (player.getFrameScore(frame - 2).size() == 1 && player.getFrameScore(frame - 1).size() == 1) {
                 player.setFrameTotalScore(frame - 2,
-                        player.getFrameTotalScore(frame - 3)
+                            player.getFrameTotalScore(frame - 3)
                                 + player.getFrameScore(frame - 2).get(0)
                                 + player.getFrameScore(frame - 1).get(0)
                                 + player.getFrameScore(frame).get(0));
 
-                if (player.getFrameScore(frame).get(0) + player.getFrameScore(frame).get(1) < 10) { // 스페어 실패
-                    //System.out.println("스페어 실패");
+                // 3-1. 현재 스페어 실패
+                if (player.getFrameScore(frame).get(0) + player.getFrameScore(frame).get(1) < 10) {
                     player.setFrameTotalScore(frame - 1,
-                            player.getFrameTotalScore(frame - 2)
+                                player.getFrameTotalScore(frame - 2)
                                     + player.getFrameScore(frame - 1).get(0)
                                     + player.getFrameScore(frame).get(0)
                                     + player.getFrameScore(frame).get(1));
 
                     player.setFrameTotalScore(frame,
-                            player.getFrameTotalScore(frame - 1)
+                                player.getFrameTotalScore(frame - 1)
                                     + player.getFrameScore(frame).get(0)
                                     + player.getFrameScore(frame).get(1));
                 }
-                if (player.getFrameScore(frame).get(0) + player.getFrameScore(frame).get(1) == 10) { // 스페어 성공
-                    //System.out.println("스페어 성공");
+
+                // 3-2. 현재 스페어 성공
+                if (player.getFrameScore(frame).get(0) + player.getFrameScore(frame).get(1) == 10) {
                     player.setFrameTotalScore(frame - 1,
-                            player.getFrameTotalScore(frame - 2)
+                                player.getFrameTotalScore(frame - 2)
                                     + player.getFrameScore(frame - 1).get(0)
                                     + player.getFrameScore(frame).get(0)
                                     + player.getFrameScore(frame).get(1));
@@ -204,43 +225,64 @@ public class BowlingService {
             }
 
             if (frame == 9) {
-                //System.out.println("마지막 라운드");
+                // 1. 전전 프레임 점수가 계산되지 않은 경우 (스트라이크)
                 if (player.getFrameTotalScore(frame - 2) == 0) {
                     player.setFrameTotalScore(frame - 2,
-                            player.getFrameTotalScore(frame - 3)
+                                player.getFrameTotalScore(frame - 3)
                                     + player.getFrameScore(frame - 2).get(0)
                                     + player.getFrameScore(frame - 1).get(0)
                                     + player.getFrameScore(frame).get(0));
                 }
 
+                // 2. 전 프레임 점수가 계산되지 않은 경우 (스트라이크 / 스페어 성공)
                 if ((player.getFrameTotalScore(frame - 1) == 0)) {
                     player.setFrameTotalScore(frame - 1,
-                            player.getFrameTotalScore(frame - 2)
+                                player.getFrameTotalScore(frame - 2)
                                     + player.getFrameScore(frame - 1).get(0)
                                     + player.getFrameScore(frame).get(0)
                                     + player.getFrameScore(frame).get(1));
                 }
 
+                // 3. 10 프레임에 보너스 샷을 못얻은 경우
                 if (player.getFrameScore(frame).size() == 2) {
                     player.setFrameTotalScore(frame,
-                            player.getFrameTotalScore(frame - 1)
+                                player.getFrameTotalScore(frame - 1)
                                     + player.getFrameScore(frame).get(0)
                                     + player.getFrameScore(frame).get(1));
                 }
 
+                // 4. 10 프레임에 보너스 샷을 얻은 경우
                 if (player.getFrameScore(frame).size() == 3) {
                     player.setFrameTotalScore(frame,
-                            player.getFrameTotalScore(frame - 1)
+                                player.getFrameTotalScore(frame - 1)
                                     + player.getFrameScore(frame).get(0)
                                     + player.getFrameScore(frame).get(1)
                                     + player.getFrameScore(frame).get(2));
                 }
-
             }
         } catch (
                 IndexOutOfBoundsException doNothing) {
         }
     }//scoreCalculator() END
 
+    /**
+     * winnerList() : 순위 구현 로직
+     * */
+    public void winnerList(List<Player> playerList) {
+        List<Player> resultList = new ArrayList<>(playerList);
+        resultList.sort(Comparator.comparing(player -> player.getFrameTotalScore(9), Comparator.reverseOrder()));
 
+        int previousScore = 0;
+        int previousRank = 1;
+        for (int i = 0; i < resultList.size(); i++) {
+            Player player = resultList.get(i);
+            if (previousScore == player.getFrameTotalScore(9) || i == 0) {
+                Message.showRank(previousRank, player);
+            } else {
+                previousRank++;
+                Message.showRank(previousRank, player);
+            }
+            previousScore = player.getFrameTotalScore(9);
+        }
+    }
 }
