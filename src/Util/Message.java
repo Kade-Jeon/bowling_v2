ce0e3f22d0c5a36bbfa863046b7a798f8021adc1
kade <zsc0248@gmail.com>
2024-03-14 04:45:15 +0900

    Feat: 1. Outro 출력 메서드 구현 2. 점수판 출력 메서드 구현 3. 순위 출력 메서드 구현 4. 예외 출력 메서드 구현

--- src/Util/Message.java
+++ src/Util/Message.java
@@ -2,78 +2,41 @@ package Util;
 
 import Service.Player;
 
-public class Message {
-    /*public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";*/
+import java.util.List;
 
-    public void introImage() {
+public class Message {
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_BLUE = "\u001B[34m";
+    private static final String ANSI_PURPLE = "\u001B[35m";
+    private static final String ANSI_CYAN = "\u001B[36m";
+
+
+    public static void intro() {
         System.out.println("""
-                 _    _        _                                   _         \s
-                | |  | |      | |                                 | |        \s
-                | |  | |  ___ | |  ___   ___   _ __ ___    ___    | |_   ___ \s
-                | |/\\| | / _ \\| | / __| / _ \\ | '_ ` _ \\  / _ \\   | __| / _ \\\s
-                \\  /\\  /|  __/| || (__ | (_) || | | | | ||  __/   | |_ | (_) |
-                 \\/  \\/  \\___||_| \\___| \\___/ |_| |_| |_| \\___|    \\__| \\___/\s
-                 
-                | ___ \\                 | |(_)             \s
-                | |_/ /  ___  __      __| | _  _ __    __ _\s
-                | ___ \\ / _ \\ \\ \\ /\\ / /| || || '_ \\  / _` |
-                | |_/ /| (_) | \\ V  V / | || || | | || (_| |
-                \\____/  \\___/   \\_/\\_/  |_||_||_| |_| \\__, |
-                                                       __/ |
-                                                      |___/\s
-                  __ _   __ _  _ __ ___    ___\s
-                 / _` | / _` || '_ ` _ \\  / _ \\
-                | (_| || (_| || | | | | ||  __/
-                 \\__, | \\__,_||_| |_| |_| \\___|
-                  __/ |                       \s
-                 |___/                        \s
+
+                 _    _  _____  _      _____  _____ ___  ___ _____     _____  _____\s
+                | |  | ||  ___|| |    /  __ \\|  _  ||  \\/  ||  ___|   |_   _||  _  |
+                | |  | || |__  | |    | /  \\/| | | || .  . || |__       | |  | | | |
+                | |/\\| ||  __| | |    | |    | | | || |\\/| ||  __|      | |  | | | |
+                \\  /\\  /| |___ | |____| \\__/\\\\ \\_/ /| |  | || |___      | |  \\ \\_/ /
+                 \\/  \\/ \\____/ \\_____/ \\____/ \\___/ \\_|  |_/\\____/      \\_/   \\___/\s
+
                                 
-                         -~~.                \s
-                         @@@*                \s
-                        !@@@@                \s
-                        .@@@@                \s
-                         @@@@-...            \s
-                         *@@@@@@@!           \s
-                         $@@@@@@@@!          \s
-                        -@@@@@@@@@@:         \s
-                        @@@@@@@@@@@@!        \s
-                       -@@@@@@@@@@@@#        \s
-                       $@@@@@@@@@,@@@@.      \s
-                       @@@@@@@@@@. @@@*      \s
-                       @@@@@@@@@@= ,@@@:     \s
-                       @@@@@@@@@@@  ,!@=     \s
-                       @@@=@@@@@@@.   !@$    \s
-                       @@@~@@@@@@@#    !@$   \s
-                       @@@:@@@@@@@;     .;:  \s
-                       @@@-@@@@@@@.          \s
-                      :@@@ @@@@@@$           \s
-                      ;@@~ ~@@@@@=           \s
-                       @@-  @@@@@.           \s
-                       @$   =@@@@            \s
-                       @.   ~@@@;            \s
-                      !@    =@@@*-           \s
-                      @=    $@@@@=           \s
-                      -!    @@@@@=           \s
-                     ::     @@@-$@           \s
-                   .@@@$    @@$ -@           \s
-                   :@@@@    @@*  $           \s
-                   :@@@@.   @$.              \s
-                   ~@@@@   #@!               \s
-                    .#*   ~@@-               \s
-                          .~~                \s
-                                
+                ______  _____  _    _  _      _____  _   _  _____     _____   ___  ___  ___ _____\s
+                | ___ \\|  _  || |  | || |    |_   _|| \\ | ||  __ \\   |  __ \\ / _ \\ |  \\/  ||  ___|
+                | |_/ /| | | || |  | || |      | |  |  \\| || |  \\/   | |  \\// /_\\ \\| .  . || |__ \s
+                | ___ \\| | | || |/\\| || |      | |  | . ` || | __    | | __ |  _  || |\\/| ||  __|\s
+                | |_/ /\\ \\_/ /\\  /\\  /| |____ _| |_ | |\\  || |_\\ \\   | |_\\ \\| | | || |  | || |___\s
+                \\____/  \\___/  \\/  \\/ \\_____/ \\___/ \\_| \\_/ \\____/    \\____/\\_| |_/\\_|  |_/\\____/\s
+                                                                    
+                             
                 """);
     }
 
-    public static void askPeople() {
+    public static void askNumberOfPlayer() {
         System.out.print("""
                 ______  _   _  _____   _____  _   _  _____   _   _  _   _ ___  _________  _____ ______   _____ ______  ______  _       ___  __   __ _____ ______  _____\s
                 | ___ \\| | | ||_   _| |_   _|| | | ||  ___| | \\ | || | | ||  \\/  || ___ \\|  ___|| ___ \\ |  _  ||  ___| | ___ \\| |     / _ \\ \\ \\ / /|  ___|| ___ \\/  ___|
@@ -91,13 +54,13 @@ public class Message {
                 \\_|  |_/\\_| |_/\\/   \\/ \\___/ \\_|  |_/ \\___/ \\_|  |_/       |_/
                                                                              \s
       
-                예) 3명인 경우 -> 3
+                \u001B[31m예) 3명인 경우 -> 3 \u001B[0m
                 """);
-        System.out.print("참가자 수를 입력해 주세요. (최대 4인) : ");
+        System.out.print(ANSI_CYAN + "참가자 수를 입력해 주세요. (최대 4인) : " + ANSI_RESET);
     }
 
-    public static void showRound(int round) {
-        switch (round) {
+    public static void showRound(int frame) {
+        switch (frame) {
             case 1 -> System.out.println("""
                                         
                     ______  _____  _   _  _   _ ______          __ \s
@@ -193,24 +156,105 @@ public class Message {
     }
 
     public static void askFirst(Player player) {
-        System.out.print(player.getName() + " 투구 결과 : ");
+        System.out.print(coloredName(player) + " 투구 결과 : ");
     }
 
     public static void askSpare(Player player) {
-        System.out.print(player.getName() + " 스페어 결과 : ");
+        System.out.print(coloredName(player) + " 스페어 결과 : ");
     }
 
     public static void askBonus(Player player) {
-        System.out.print(player.getName() + " 보너스 결과 : ");
+        System.out.print(coloredName(player) + " 보너스 결과 : ");
     }
 
+    public static void showDisplayFrame(List<Player> playerList) {
+        System.out.printf("""
+        |---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|
+        |%9s|%9d|%9d|%9d|%9d|%9d|%9d|%9d|%9d|%9d|%9d|
+        |---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|
+        """, " ",1,2,3,4,5,6,7,8,9,10);
 
-    /*System.out.println(ANSI_RED + "이 텍스트는 빨간색입니다." + ANSI_RESET);
-    System.out.println(ANSI_GREEN + "이 텍스트는 녹색입니다." + ANSI_RESET);
-    System.out.println(ANSI_BLUE + "이 텍스트는 파란색입니다." + ANSI_RESET);
-    System.out.println(ANSI_PURPLE + "이 텍스트는 보라색입니다." + ANSI_RESET);
-    System.out.println(ANSI_YELLOW + "이 텍스트는 노란색입니다." + ANSI_RESET);
-    System.out.println(ANSI_CYAN + "이 텍스트는 청록색입니다." + ANSI_RESET);
-    System.out.println(ANSI_BLACK + "이 텍스트는 검은색입니다." + ANSI_RESET);
-    System.out.println(ANSI_WHITE + "이 텍스트는 흰색입니다." + ANSI_RESET);*/
+        String scoreTable = """
+                |%9s|%9s|%9s|%9s|%9s|%9s|%9s|%9s|%9s|%9s|%9s|
+                |---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|
+                """;
+
+        String calTable = """
+                |%9s|%9s|%9s|%9s|%9s|%9s|%9s|%9s|%9s|%9s|%9s|
+                |---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|
+                """;
+
+        for (Player player : playerList) {
+            System.out.printf(scoreTable, coloredName(player)+ "  ",
+                    player.getDisplayFrame(0, player),
+                    player.getDisplayFrame(1, player),
+                    player.getDisplayFrame(2, player),
+                    player.getDisplayFrame(3, player),
+                    player.getDisplayFrame(4, player),
+                    player.getDisplayFrame(5, player),
+                    player.getDisplayFrame(6, player),
+                    player.getDisplayFrame(7, player),
+                    player.getDisplayFrame(8, player),
+                    player.getDisplayFrame(9, player)
+            );
+
+            System.out.printf(calTable, " ",
+                    player.getFrameTotalScore(0),
+                    player.getFrameTotalScore(1),
+                    player.getFrameTotalScore(2),
+                    player.getFrameTotalScore(3),
+                    player.getFrameTotalScore(4),
+                    player.getFrameTotalScore(5),
+                    player.getFrameTotalScore(6),
+                    player.getFrameTotalScore(7),
+                    player.getFrameTotalScore(8),
+                    player.getFrameTotalScore(9)
+            );
+        }
+    }
+
+    public static void outro() {
+        System.out.println("""
+                                
+                 _____   ___  ___  ___ _____   _____  _   _ ______\s
+                |  __ \\ / _ \\ |  \\/  ||  ___| |  ___|| \\ | ||  _  \\
+                | |  \\// /_\\ \\| .  . || |__   | |__  |  \\| || | | |
+                | | __ |  _  || |\\/| ||  __|  |  __| | . ` || | | |
+                | |_\\ \\| | | || |  | || |___  | |___ | |\\  || |/ /\s
+                 \\____/\\_| |_/\\_|  |_/\\____/  \\____/ \\_| \\_/|___/ \s
+                                
+                """);
+    }
+
+    private static String coloredName(Player player) {
+        switch (player.getName()) {
+            case "Player1" -> {
+                return ANSI_GREEN + player.getName() + ANSI_RESET;
+            }
+            case "Player2" -> {
+                return ANSI_YELLOW + player.getName() + ANSI_RESET;
+            }
+            case "Player3" -> {
+                return ANSI_BLUE + player.getName() + ANSI_RESET;
+            }
+            case "Player4" -> {
+                return ANSI_PURPLE + player.getName() + ANSI_RESET;
+            }
+            default -> {
+                return player.getName();
+            }
+        }
+    }
+
+    public static void showRank(int previousRank, Player player) {
+        System.out.print(previousRank +"위 : ");
+        System.out.println(coloredName(player) + " " + player.getFrameTotalScore(9));
+    }
+
+    public static void InputMismatchExceptionPrint() {
+        System.out.println(ANSI_RED + "숫자만 입력 가능합니다." + ANSI_RESET);
+    }
+    public static void InputOutOfRangeExceptionPrint(String errorMessage) {
+        System.out.println(ANSI_RED + errorMessage + ANSI_RESET);
+    }
 }
