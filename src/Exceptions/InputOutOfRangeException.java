966c4a77aa6869aca72c94ff5db87db4ae84a7d7
kade <zsc0248@gmail.com>
2024-03-14 04:43:24 +0900

    Feat : 1. 입력 값 범위 초과 시 사용할 커스텀 예외 구현 2. 경고 시, 빨간색으로 출력 위한 ANSI 코드 추가

--- src/Exceptions/InputOutOfRangeException.java
+++ src/Exceptions/InputOutOfRangeException.java
@@ -3,7 +3,10 @@ package Exceptions;
 import java.util.InputMismatchException;
 
 public class InputOutOfRangeException extends InputMismatchException {
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_RED = "\u001B[31m";
+
     public InputOutOfRangeException(String s) {
-        super(s);
+        super(ANSI_RED + s + ANSI_RESET);
     }
 }
