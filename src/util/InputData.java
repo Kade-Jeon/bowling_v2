80e3238dc90c5d1aa39949008ea41c1cffa608ec
kade <zsc0248@gmail.com>
2024-03-21 19:11:57 +0900

    Refactor : 1. numberOfPin 하나로 처리하던 것을 초구 후 점수판 보여주도록 구현하기 위해 first, spare, bonus 세 개로 분리하고 각각 예외처리 구현

--- src/util/InputData.java
+++ src/util/InputData.java
@@ -1,6 +1,7 @@
 package util;
 
 import exceptions.InputOutOfRangeException;
+import service.Player;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -24,7 +25,7 @@ public class InputData {
                 Message.InputOutOfRangeExceptionPrint(e.getMessage());
             } catch (InputMismatchException e) {
                 Message.InputMismatchExceptionPrint();
-                scanner.nextLine();
+                cleanBuffer();
             }
         }
         return numberOfPlayers;
@@ -34,7 +35,71 @@ public class InputData {
         return scanner.nextInt();
     }
 
+    public int resultOfFirst(Player player) {
+        int first;
+        while (true) {
+            try {
+                Message.askFirst(player);
+                first = scanner.nextInt();
+                if (0 > first || first > 10) { // First 입력 오류
+                    throw new InputOutOfRangeException("투구 결과는 0 ~ 10 사이의 값만 입력 가능합니다.");
+                }
+                return first;
+            } catch (InputOutOfRangeException e) {
+                Message.InputOutOfRangeExceptionPrint(e.getMessage());
+            } catch (InputMismatchException e) {
+                Message.InputMismatchExceptionPrint();
+                cleanBuffer(); // 버퍼를 비워 주지 않으면 무한 반복이 일어 난다고 함...
+            }
+        }
+    }
+
+    public int resultOfSpare(Player player, int first) {
+        int spare;
+        while (true) {
+            try {
+                Message.askSpare(player);
+                spare = scanner.nextInt();
+
+                if (first == 10 && (0 > spare || spare > 10)) { // 10 Frame 인 경우
+                    throw new InputOutOfRangeException("스페어 결과는 0 ~ 10 사이의 값만 입력 가능합니다. \n재입력 바랍니다.");
+                }
+                if (first < 10 && (0 > spare || spare > 10 || first + spare > 10)) { // 10 Frame 인 경우
+                    throw new InputOutOfRangeException("스페어 결과는 0 ~ " + (10 - first) + " 사이의 값만 입력 가능합니다. \n재입력 바랍니다.");
+                }
+                return spare;
+            } catch (InputOutOfRangeException e) {
+                Message.InputOutOfRangeExceptionPrint(e.getMessage());
+            } catch (InputMismatchException e) {
+                Message.InputMismatchExceptionPrint();
+                cleanBuffer(); // 버퍼를 비워 주지 않으면 무한 반복이 일어 난다고 함...
+            }
+        }
+    }
+
+    public int resultOfBonus(Player player, int first, int spare) {
+        int bonus;
+        while (true) {
+            try {
+                Message.askBonus(player);
+                bonus = scanner.nextInt();
+
+                if (first == 10 && spare < 10 && (0 > bonus || bonus > 10 || spare + bonus > 10)) { // Bonus 입력 오류
+                    throw new InputOutOfRangeException("보너스 결과는 0 ~ " + (10 - spare) + " 사이의 값만 입력 가능합니다. \n재입력 바랍니다.");
+                }
+
+                return bonus;
+            } catch (InputOutOfRangeException e) {
+                Message.InputOutOfRangeExceptionPrint(e.getMessage());
+            } catch (InputMismatchException e) {
+                Message.InputMismatchExceptionPrint();
+                cleanBuffer(); // 버퍼를 비워 주지 않으면 무한 반복이 일어 난다고 함...
+            }
+        }
+    }
+
     public void cleanBuffer() {
         scanner.nextLine();
     }
+
 }
